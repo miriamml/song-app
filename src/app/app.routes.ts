d8e874c240a7e9fcae29454ccf960890168f342a
MiriamML24 <39255953+MiriamML24@users.noreply.github.com>
2025-02-18 21:47:40 +0100

    fix/configs files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {SongListComponent} from './song/component/song-list/song-list.component';
+import {SongEditComponent} from './song/component/edit-song/edit-song.component';
 
 export const routes: Routes = [
-  { path: 'songs', component: SongListComponent },
+    { path: 'songs', component: SongListComponent },
+    { path: 'songs/:id', component: SongEditComponent },
 ];
